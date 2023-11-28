5353afe747bca8c1ef244306cc208ae7b60868fe
masudpro121 <masud.pro.121@gmail.com>
2023-11-28 11:40:03 +0600

    all image and svg imported

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Layout from "@/components/Layout"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+      <div>homes</div>
+  )
+}
